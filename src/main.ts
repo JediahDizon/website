c85f58311ae8c473fd27cf54b9c359f5f3d5c0ca
Jediah Dizon <JediahJosuah@gmail.com>
2020-06-29 11:48:25 -0600

    Spaces to tabs

--- src/main.ts
+++ src/main.ts
@@ -8,34 +8,34 @@ import { NgModuleRef, ApplicationRef } from '@angular/core';
 import { createNewHosts } from '@angularclass/hmr';
 
 const hmrBootstrap = (
-  module: any,
-  bootstrap: () => Promise<NgModuleRef<any>>
+	module: any,
+	bootstrap: () => Promise<NgModuleRef<any>>
 ) => {
-  let ngModule: NgModuleRef<any>;
-  module.hot.accept();
-  bootstrap().then(mod => (ngModule = mod));
-  module.hot.dispose(() => {
-    const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
-    const elements = appRef.components.map(c => c.location.nativeElement);
-    const makeVisible = createNewHosts(elements);
-    ngModule.destroy();
-    makeVisible();
-  });
+	let ngModule: NgModuleRef<any>;
+	module.hot.accept();
+	bootstrap().then(mod => (ngModule = mod));
+	module.hot.dispose(() => {
+		const appRef: ApplicationRef = ngModule.injector.get(ApplicationRef);
+		const elements = appRef.components.map(c => c.location.nativeElement);
+		const makeVisible = createNewHosts(elements);
+		ngModule.destroy();
+		makeVisible();
+	});
 };
 
 if (environment.production) {
-  enableProdMode();
+	enableProdMode();
 }
 
 const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.hmr) {
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
+	if (module['hot']) {
+		hmrBootstrap(module, bootstrap);
+	} else {
+		console.error('HMR is not enabled for webpack-dev-server!');
+		console.log('Are you using the --hmr flag for ng serve?');
+	}
 } else {
-  bootstrap().catch(err => console.log(err));
+	bootstrap().catch(err => console.log(err));
 }
